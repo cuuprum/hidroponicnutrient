60cc6515cfbe518f475ceb13d5d30bf739d0d07e
cuuprum <tembagajingga@gmail.com>
2019-07-05 04:20:32 +0700

    Fix servername

--- model/Config.php
+++ model/Config.php
@@ -1,6 +1,6 @@
 <?php
     // Define connection
-    define('DB_SERVER', 'srvcuuprum');
+    define('DB_SERVER', 'srvcuuprum.database.windows.net)');
     define('DB_USERNAME', 'nlbe7a7riwr');
     define('DB_PASSWORD', 'glvothoh3moy2Chltoc3');
     define('DB_NAME', 'dbhidroponicnutrient');
